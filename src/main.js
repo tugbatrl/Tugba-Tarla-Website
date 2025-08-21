f5d65d873324ff287aee7e0746829fe5a0d10a9f
Tugba Tarla <tubatarla3883@gmail.com>
2025-08-22 01:52:16 +0300

    arayuz tasarlandı ve responsive hale getirildi

--- src/main.js
+++ src/main.js
@@ -1,24 +1,24 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+//task-manager
+document.getElementById("task-manager-repo").addEventListener("click" , function() {
+  window.open("https://github.com/tugbatrl/Task-Manager-Project" , "_blank");
+});
 
-setupCounter(document.querySelector('#counter'))
+//hackathon
+document.getElementById("hackathon-repo").addEventListener("click", function(){
+  window.open("https://github.com/tugbatrl/HACKATHON25-Duygu-Pusulasi");
+});
+
+document.getElementById("hackathon-link").addEventListener("click" , function() {
+  window.open("https://duygu-pusulasi.onrender.com/" , "_blank");
+});
+
+//website
+document.getElementById("tugba-tarla-repo").addEventListener("click", function(){
+  window.open("https://github.com/tugbatrl/Tugba-Tarla-Website");
+});
+
+document.getElementById("tugba-tarla-link").addEventListener("click" , function() {
+  window.open("https://github.com/tugbatrl/Tugba-Tarla-Website" , "_blank");
+});
